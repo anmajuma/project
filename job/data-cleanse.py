515aca34d3f3bae7cdd418ad29c0969cb6c2082f
anmajuma <anmajum@live.com>
2022-07-10 20:38:24 +0530

    added cmd line args

--- job/data-cleanse.py
+++ job/data-cleanse.py
@@ -1,10 +1,20 @@
+import sys
 from app.io.spark import spark_build
 from app.config.helper import read_config
 from pyspark.sql.types import *
 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+print("\nName of filepath:", sys.argv[1])
+
+
 spark = spark_build("data-cleanse")
 config = read_config()
-landingpath = config['LandingLayerSettings']['landingpath']
+landingpath = config['LandingLayerSettings']['landingpath'] + "/" + sys.argv[1]
 # Read from HDFS
 df_load = spark.read.option("header","true").parquet(landingpath)
 df_load.show()
