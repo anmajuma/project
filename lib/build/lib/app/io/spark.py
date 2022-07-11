76f5946a676471d3fb754f85216625f24e488819
anmajuma <you@example.com>
2022-07-11 18:13:44 +0530

    added Delta lake

--- lib/build/lib/app/io/spark.py
+++ lib/build/lib/app/io/spark.py
@@ -1,6 +1,14 @@
 from pyspark.sql import SparkSession
+from delta import configure_spark_with_delta_pip
 
 def spark_build(app_name):
-  spark = SparkSession.builder.config("spark.jars", "jars/mysql-connector-java-8.0.29.jar").appName(app_name).getOrCreate()
+  builder = (
+  SparkSession.builder
+    .appName(app_name)
+    .config('spark.sql.extensions', 'io.delta.sql.DeltaSparkSessionExtension')
+    .config('spark.sql.catalog.spark_catalog', 'org.apache.spark.sql.delta.catalog.DeltaCatalog')
+    .config("spark.jars", "jars/mysql-connector-java-8.0.29.jar")
+)
+  spark = configure_spark_with_delta_pip(builder).getOrCreate()
   print("Session Created")
   return spark
